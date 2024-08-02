e1516a3f35f1f265807b10c0fe7a9dabb81a49cf
Raifran Alves <raifransalves@gmail.com>
2024-08-02 18:05:44 -0300

    Atualizar projeto

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+function alteraCor(event) {
+    event.style.background = "#F2E7DC";
+    event.style.border = "none";
+}
+
+function desfazerCor(event) {
+    event.style.background = "white";
+    event.style.border = "none";
+    event.style.color="#0D0D0D";
+}
