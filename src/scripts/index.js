1726aec27ab9e5cc4fc0618937d831920d0ab217
Raifran Alves <raifransalves@gmail.com>
2025-04-01 17:24:28 -0300

    Atualiza projeto

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,17 @@
-function alteraCor(event) {
-    event.style.background = "#F2E7DC";
-    event.style.border = "none";
-}
+let formulario = document.querySelector('form');
+let nome = document.getElementById('nome');
+let data = document.getElementById('dia');
+let horario = document.getElementById('horario');
+let mensagem = document.getElementsByClassName('mensagem');
 
-function desfazerCor(event) {
-    event.style.background = "white";
-    event.style.border = "none";
-    event.style.color="#0D0D0D";
-}
+
+formulario.addEventListener('submit', (evento) => {
+    if (nome.value === "" && data.value === "" && horario.value === "") {
+        evento.preventDefault();
+        mensagem.textContent = 'Preencha todos os campos';
+        console.log(mensagem);
+    } else {
+        mensagem.textContent = "Formulario enviado!";
+
+    }
+});
